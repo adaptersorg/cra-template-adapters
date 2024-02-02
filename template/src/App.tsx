6247ea399544143d9581dc1b8876a945277efdaa
Ubinquitous <ubinquitous1@gmail.com>
2024-02-02 13:49:48 +0900

    fix : 자잘한 버그 수정

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,18 +1,7 @@
-import * as R from 'allFiles'
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { RecoilRoot } from 'recoil'
 
 const App = () => {
-	return (
-		<RecoilRoot>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<R.Home />} />
-				</Routes>
-			</BrowserRouter>
-		</RecoilRoot>
-	)
+	return <div></div>
 }
 
 export default App
